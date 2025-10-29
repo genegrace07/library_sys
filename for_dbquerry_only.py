a66c519db0c7850e5752784ac4ffe287a4e69008
mark anthony dador <dador.markanthony@gmail.com>
2025-10-29 21:52:42 +0800

    remove for dbquerry to env

---
+++ for_dbquerry_only.py
@@ -0,0 +1,40 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+import json
+
+load_dotenv()
+
+db = mysql.connector.connect(
+    host = os.getenv('dbhost'),
+    user = os.getenv('dbuser'),
+    password = os.getenv('dbpassword'),
+    database = os.getenv('dbname')
+)
+
+dbcursor = db.cursor()
+dbcursor.execute('show tables from library')
+result_querry = dbcursor.fetchall()
+#print(result_querry)
+
+#import json data to mysql
+books_json = 'books.json'
+with open(books_json,"r") as f:
+    data = json.load(f)
+
+for l in data:
+    #print(l)
+    dbcursor.execute("""insert into books(book_id,title,author,status) values(%s,%s,%s,%s)
+                     ON DUPLICATE KEY UPDATE
+                     title=values(title),
+                     author=values(author),
+                     status=values(status)
+                     """,(l['book_id'],l['title'],l['author'],l['status']))
+
+db.commit()
+dbcursor.close()
+db.close()
+
+
+
+
