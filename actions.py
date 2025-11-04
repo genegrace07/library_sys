afd6d97ecb5a5d50533057b629270cd0fa38514b
mark anthony dador <dador.markanthony@gmail.com>
2025-11-04 22:01:40 +0800

    add add.html for add books, ongoing

--- actions.py
+++ actions.py
@@ -2,9 +2,9 @@ from flask import redirect,render_template,url_for,Blueprint,request
 
 action = Blueprint("action",__name__)
 
-@action.route('/available',methods=['POST','GET'])
-def available():
-    pass
+@action.route('/add',methods=['POST','GET'])
+def add():
+    return render_template('add.html')
     # if request.method == 'POST':
     #     request.form['']
 
